3a1485de6849313bc33b43827c51892fcbea85bf
leonardo <44080750+leojimenezp@users.noreply.github.com>
2018-11-15 19:26:21 -0500

    bd
    .

---
+++ bdhotel.sql
@@ -0,0 +1,52 @@
+/*
+SQLyog Community v8.71 
+MySQL - 5.5.5-10.1.25-MariaDB : Database - trivago
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`trivago` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `trivago`;
+
+/*Table structure for table `habitaciones` */
+
+DROP TABLE IF EXISTS `habitaciones`;
+
+CREATE TABLE `habitaciones` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `habitacion` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `habitaciones` */
+
+insert  into `habitaciones`(`id`,`habitacion`) values (1,'101'),(3,'103');
+
+/*Table structure for table `usuarios` */
+
+DROP TABLE IF EXISTS `usuarios`;
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(50) DEFAULT NULL,
+  `pass` varchar(50) DEFAULT NULL,
+  `rol` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `usuarios` */
+
+insert  into `usuarios`(`id`,`user`,`pass`,`rol`) values (1,'leonardo','12345','admin'),(2,'tefa','lamejor','vendedor');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
